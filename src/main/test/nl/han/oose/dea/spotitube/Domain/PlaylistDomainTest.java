ddd68861045f1d8e2c4210b079bcdaa0e679e85d
Ruben <ruben_eppink@live.nl>
2020-03-18 14:14:29 +0100

    playlistdomain fixed

--- src/main/test/nl/han/oose/dea/spotitube/Domain/PlaylistDomainTest.java
+++ src/main/test/nl/han/oose/dea/spotitube/Domain/PlaylistDomainTest.java
@@ -25,7 +25,7 @@ class PlaylistDomainTest {
     }
 
     @Test
-    void testGetAll() {
+    void testGetAllReturnsPlaylistsDTO() {
         // Setup
 
         // Configure PlaylistDAO.getAll(...).
@@ -42,7 +42,55 @@ class PlaylistDomainTest {
     }
 
     @Test
-    void testDelete() {
-    
+    void testGetAllCallsGetAll() {
+        // Setup
+
+        // Configure PlaylistDAO.getAll(...).
+        final PlaylistsDTO playlistsDTO = new PlaylistsDTO(Arrays.asList(
+                new PlaylistDTO(0, "name", false, Arrays.asList(
+                        new TrackDTO(0, "title", "performer", 0, "album", 0, "publicationDate", "description", false)))), 0);
+        when(playlistDomainUnderTest.playlistDAO.getAll()).thenReturn(playlistsDTO);
+
+        // Run the test
+        final PlaylistsDTO result = playlistDomainUnderTest.getAll();
+
+        // Verify the results
+        verify(mockedPlaylistDAO).getAll();
+    }
+
+
+
+    @Test
+    void testDeleteCallsPlaylistDAODelete() {
+        // Setup
+
+        // Configure PlaylistDAO.getAll(...).
+        final PlaylistsDTO playlistsDTO = new PlaylistsDTO(Arrays.asList(
+                new PlaylistDTO(0, "name", false, Arrays.asList(
+                        new TrackDTO(0, "title", "performer", 0, "album", 0, "publicationDate", "description", false)))), 0);
+        when(playlistDomainUnderTest.playlistDAO.getAll()).thenReturn(playlistsDTO);
+
+        // Run the test
+        final PlaylistsDTO result = playlistDomainUnderTest.delete(0);
+
+        // Verify the results
+        verify(mockedPlaylistDAO).delete(0);
+    }
+
+    @Test
+    void testDeleteReturnsPlaylistsDTO() {
+        // Setup
+
+        // Configure PlaylistDAO.getAll(...).
+        final PlaylistsDTO playlistsDTO = new PlaylistsDTO(Arrays.asList(
+                new PlaylistDTO(0, "name", false, Arrays.asList(
+                        new TrackDTO(0, "title", "performer", 0, "album", 0, "publicationDate", "description", false)))), 0);
+        when(playlistDomainUnderTest.playlistDAO.getAll()).thenReturn(playlistsDTO);
+
+        // Run the test
+        final PlaylistsDTO result = playlistDomainUnderTest.delete(0);
+
+        // Verify the results
+        Assertions.assertEquals(playlistsDTO, result);
     }
 }
